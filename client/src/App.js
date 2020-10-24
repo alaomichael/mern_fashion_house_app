884c79b707102eabd1c1162cf773bf319915305a
Michael Alao <contactleomax@gmail.com>
2020-10-24 18:44:36 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,3 @@
-// import React, { Component } from "react";
-// import { BrowserRouter as Router, Route } from "react-router-dom";
-// import CreateTodo from "./components/create-todo.component";
-// import EditTodo from "./components/edit-todo.component";
-// import TodosList from "./components/todos-list.component";
-// import 'bootstrap/dist/css/bootstrap.css';
-// //import logo from "./logo.svg";
-// import ShowTodo from "./components/show-todo.component";
-// import Navbar from "./components/navbar.component"
-// import CreateUser from "./components/create-user.components";
-// import SignIn from './components/SignIn'
-// import SignUp from "./components/SignUp";
-
-// class App extends Component {
-//  render() {
-//        return (
-//          <Router>
-//          <div className="container">
-//      <Navbar/>
-//       <br />
-// <Route path="/" exact component={TodosList} />
-// <Route path="/edit/:id" component={EditTodo} /> 
-// <Route path="/create" component={CreateTodo} /> 
-// <Route path="/show/:id" component={ ShowTodo } /> 
-// <Route path="/user" component={ CreateUser } />     
-// <Route path="/signin" component={ SignIn} />  
-// <Route path="/signup" component={ SignUp } />   
-// </div> 
-
-//       </Router>    
-//       );
-//       }
-//     }
-
-//   export default App;
-
-
-// import { BrowserRouter as Router, Route } from "react-router-dom";
-
 import CreateTodo from "./components/create-todo.component";
 import TodosList from "./components/todos-list.component";
 import 'bootstrap/dist/css/bootstrap.css';
@@ -51,6 +12,12 @@ import CreateUser from './components/create-user.components';
 
 
 function App() {
+
+  const [todos,setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage,setCurrentPage] = useState(1);
+  const [todosPerPage, setTodosPerPage] = useState(5);
+  
   return (
     <BrowserRouter>
       <div className="container">
