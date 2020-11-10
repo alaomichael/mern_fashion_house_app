591cb933ea65b3862d9625607ee0641780307db3
Michael Alao <contactleomax@gmail.com>
2020-11-10 04:01:06 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,39 +3,37 @@ import TodosList from "./components/todos-list.component";
 import 'bootstrap/dist/css/bootstrap.css';
 import ShowTodo from "./components/show-todo.component";
 import Navbar from "./components/navbar.component"
-import React, {useState, useEffect } from 'react';
+import Home from "./components/home.component"
+import React from 'react';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
 import EditTodo from './components/edit-todo.component';
 import CreateUser from './components/create-user.components';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect,Switch } from 'react-router-dom';
 
-function App() {
 
-  const [todos,setTodos] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [currentPage,setCurrentPage] = useState(1);
-  const [todosPerPage, setTodosPerPage] = useState(5);
-  
+function App()  {
   return (
     <BrowserRouter>
       <div className="container">
         <Navbar />
         <br />
         <Switch>
-          <Route path="/" exact component={ TodosList } />
-          <Route path="/edit/:id" component={ EditTodo } />
+        <Route path="/" exact component={ Home } />  
+         <Route path="/list" component={ TodosList } />  
+          <Route path="/list/edit/:id" component={ EditTodo } />
           <Route path="/create" component={ CreateTodo } />
-          <Route path="/show/:id" component={ ShowTodo } />
+          <Route path="/list/show/:id" component={ ShowTodo } />
           <Route path="/user" component={ CreateUser } />
           <Route path="/signin" component={ SignIn } />
           <Route path="/signup" component={ SignUp } />
         </Switch>
+        <div>
+        
+        </div>
       </div>
     </BrowserRouter>
-
   );
 }
 
 export default App;
-
