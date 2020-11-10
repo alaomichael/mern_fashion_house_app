58a3f8406a578b7e66df23ad098c672281f7cc74
Michael Alao <contactleomax@gmail.com>
2020-11-10 05:32:10 +0100

    Update SignedOutLinks.js

--- client/src/components/SignedOutLinks.js
+++ client/src/components/SignedOutLinks.js
@@ -5,9 +5,7 @@ import { Card } from 'reactstrap';
 const SignedOutLinks = () => {
     return (
         <>
-        <Card>
-         <p>This application is for keeping the records of your customers measurement in an easily accessible platform.</p>
-        <p><b>Sign Up Now or Sign In to Enjoy this service.</b></p>
+      
             <ul className="right navbar-nav mr-auto">
                 <li className="navbar-item">
                     <Link to="/signup" className="nav-link">Sign Up</Link>
@@ -15,10 +13,7 @@ const SignedOutLinks = () => {
                 <li className="navbar-item">
                     <Link to="/signin" className="nav-link">Login</Link>
                 </li>
-            </ul>
-            <p><strong>You can contact us on +234-703-368-0599 or contactleomax@gmail.com</strong></p>
-        </Card>
-       
+            </ul>     
            
         </>
     )
