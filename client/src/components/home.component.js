d1b18dcdf6a2d372187aa82d08a1d4b0d320be42
Michael Alao <contactleomax@gmail.com>
2020-11-13 19:53:34 +0100

    Update home.component.js

--- client/src/components/home.component.js
+++ client/src/components/home.component.js
@@ -7,20 +7,18 @@ import { Redirect } from 'react-router-dom';
 
 const Home = (props) => {
     const { auth} = props;
-    const links = auth.uid ? <Redirect to='/list' /> : <SignedOutLinks />
+    const links = auth.uid ? <Redirect to='/' /> : <SignedOutLinks />
 
   return (
     <>
+    <h1>Welcome to Cloth Measurement Record Application</h1>
     <Card>
          <p>This application is for keeping the records of your customers measurement in an easily accessible platform.</p>
         <p><b>Sign Up Now or Sign In to Enjoy this service.</b></p>
           
             <p><strong>You can contact us on +234-703-368-0599 or contactleomax@gmail.com</strong></p>
         </Card>
-        
-        {links}
-        
-        
+        {links}        
     </>
   );
 }
@@ -28,8 +26,7 @@ const Home = (props) => {
 const mapStateToProps = (state) => {
     console.log(state);
     return {
-        auth: state.firebase.auth,
-        profile: state.firebase.profile
+        auth: state.firebase.auth
     }
 
 }
